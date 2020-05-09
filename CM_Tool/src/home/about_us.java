ad6878470306bc97d31051d51f7883f6a39ecaf2
y3s1-2020 <61650233+y3s1-2020@users.noreply.github.com>
2020-05-10 03:49:04 +0530

    Add new buttons to about_us page

--- CM_Tool/src/home/about_us.java
+++ CM_Tool/src/home/about_us.java
@@ -6,8 +6,10 @@
 package home;
 
 import home.CM_ToolHOME;
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.net.URI;
 import java.util.Locale;
 
 /**
@@ -45,7 +47,8 @@ public class about_us extends javax.swing.JFrame {
         jPanel4 = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
         jPanel5 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -132,27 +135,45 @@ public class about_us extends javax.swing.JFrame {
 
         jPanel5.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/facebook.png"))); // NOI18N
-        jLabel1.setMaximumSize(new java.awt.Dimension(64, 64));
-        jLabel1.setMinimumSize(new java.awt.Dimension(64, 64));
-        jLabel1.setPreferredSize(new java.awt.Dimension(64, 64));
-        jLabel1.setVerifyInputWhenFocusTarget(false);
+        jButton1.setFont(new java.awt.Font("Ubuntu", 1, 36)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(57, 141, 239));
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/circular.png"))); // NOI18N
+        jButton1.setText("facebook");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setFont(new java.awt.Font("Ubuntu", 1, 36)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(75, 136, 251));
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/social-media.png"))); // NOI18N
+        jButton2.setText("linked in");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
         jPanel5Layout.setHorizontalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel5Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel5Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel5Layout.createSequentialGroup()
-                .addGap(39, 39, 39)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(245, Short.MAX_VALUE))
+                .addGap(86, 86, 86)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addContainerGap(155, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -208,6 +229,24 @@ public class about_us extends javax.swing.JFrame {
         about_us.this.dispose();  //Exit frome the about us page
     }//GEN-LAST:event_jLabel3MouseClicked
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        Desktop browser = Desktop.getDesktop();      //link with git hub page
+        try {
+            browser.browse(new URI("https://facebook.com"));
+        } catch (Exception err) {
+            System.out.println(err);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        Desktop browser = Desktop.getDesktop();      //link with git hub page
+        try {
+            browser.browse(new URI("https://linkedin.com"));
+        } catch (Exception err) {
+            System.out.println(err);
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -245,7 +284,8 @@ public class about_us extends javax.swing.JFrame {
 
     CM_ToolHOME cm;
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
